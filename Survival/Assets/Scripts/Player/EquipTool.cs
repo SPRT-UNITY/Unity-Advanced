614b61f43e75e88d80f99becdcc9eb477f2fb6f8
Siryu <siryu2409@gmail.com>
2024-02-02 19:47:24 +0900

    1-9 Resource collecting

--- Survival/Assets/Scripts/Player/EquipTool.cs
+++ Survival/Assets/Scripts/Player/EquipTool.cs
@@ -39,6 +39,25 @@ public class EquipTool : Equip
         attacking = false;
     }
 
+    public void OnHit() 
+    {
+        Ray ray = camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, attackDistance)) 
+        {
+            if(doesGatherResources && hit.collider.TryGetComponent(out Resource resource)) 
+            {
+                resource.Gather(hit.point, hit.normal);
+            }
+
+            if(doesDealDamage && hit.collider.TryGetComponent(out IDamagable damagable)) 
+            {
+                damagable.TakePhysicalDamage(damage);
+            }
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
